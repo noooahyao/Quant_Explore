507ea271235b294606559ce93906e84ea4092209
Noah Yao <yaoyidian@126.com>
2023-06-06 00:37:37 +0800

    update RRG logic

---
+++ Prediction/RRG Rules.md
@@ -0,0 +1,32 @@
+# Business Rules
+
+**Relative strength**: You could set a rule to buy ETFs that are in the leading quadrant and sell ETFs that are in the lagging quadrant.
+
+**Momentum**: You could set a rule to buy ETFs that are moving from the improving quadrant to the leading quadrant and sell ETFs that are moving from the weakening quadrant to the lagging quadrant.
+
+**Diversification**: For example, you could set a rule to buy one ETF from each quadrant to ensure exposure to different sectors.
+
+**Risk management**: You could set a rule to sell ETFs that move from the leading quadrant to the weakening or lagging quadrant, as this could indicate a loss of momentum or a shift in market conditions
+
+**Buy strong candidates in leading quadrant**: Identify assets that are in the leading quadrant and have strong momentum, then buy these assets as they are expected to outperform the market in the short term.
+
+**Sell weak candidates in lagging quadrant**: Identify assets that are in the lagging quadrant and have weak momentum, then sell these assets as they are expected to underperform the market in the short term.
+
+**Buy assets moving from weakening to leading quadrant**: Identify assets that are transitioning from the weakening quadrant to the leading quadrant, then buy these assets as they have the potential to gain momentum and outperform the market in the near future.
+
+**Buy assets moving from lagging to improving quadrant**: Identify assets that are transitioning from the lagging quadrant to the improving quadrant, then buy these assets as they are expected to improve and potentially outperform the market in the medium to long term.
+
+**Take profits on assets moving from leading to weakening quadrant**: Identify assets that are transitioning from the leading quadrant to the weakening quadrant, then take profits on these assets as they are likely to experience a decrease in momentum and underperform the market in the near future.
+
+**Use stop losses**: Set stop loss orders to limit potential losses in case the market moves against the expected trend
+
+# Mathmatical Rules
+
+## Area
+1. **Leading** - RS_Momentum > 0 and RS_Ratio >0
+
+2. **Improving** - RS_Momentum > 0 and RS_Ratio < 0
+
+3. **Lagging** - RS_Momentum < 0 and RS_Ratio < 0
+
+4. **Weakning** - RS_Momentum < 0 and RS_Ratio >0
--- Prediction/rrg_toolkit.py
+++ Prediction/rrg_toolkit.py
@@ -5,8 +5,25 @@ import matplotlib as mpl
 from datetime import datetime
 import numpy as np
 from typing import List
-from pptx import Presentation
-from pptx.util import Inches
+
+def calculate_rs_ratios(prices, benchmark):
+    """
+    Calculate the RS ratios relative to a benchmark.
+    Parameters:
+    prices (pd.DataFrame): A DataFrame containing the ETF prices.
+    benchmark (str): The ticker symbol of the benchmark.
+    Returns:
+    pd.DataFrame: A DataFrame containing the RS ratios.
+    """
+    benchmark_prices = prices[benchmark]
+    rs_ratios = pd.DataFrame()
+    for etf in prices.columns:
+        rs_ratio = prices[etf] / benchmark_prices
+        rs_ratios[etf] = rs_ratio
+
+    rs_ratios.columns = pd.MultiIndex.from_product([rs_ratios.columns, ['RS Ratio']])
+
+    return rs_ratios
 
 def calc_jdk_rs_ratio(data: pd.Series) -> pd.Series:
     """
@@ -63,161 +80,6 @@ def calculate_jdk_rs(data: pd.Series) -> pd.Series:
 
     return jdk_rs
 
-
-def get_etf_prices(etfs, start_date, end_date):
-    """
-    Retrieve ETF prices from Yahoo Finance.
-    Parameters:
-    etfs (list): A list of ETF ticker symbols.
-    start_date (str): The start date in 'YYYY-MM-DD' format.
-    end_date (str): The end date in 'YYYY-MM-DD' format.
-    Returns:
-    pd.DataFrame: A DataFrame containing the ETF prices.
-    """
-    prices = pd.DataFrame()
-    for etf in etfs:
-        try:
-            data = yf.download(etf, start=start_date, end=end_date)
-            prices[etf] = data["Close"]
-        except Exception as e:
-            print(f"Error retrieving data for {etf}: {e}")
-
-    return prices
-
-
-def calculate_rs_ratios(prices, benchmark):
-    """
-    Calculate the RS ratios relative to a benchmark.
-    Parameters:
-    prices (pd.DataFrame): A DataFrame containing the ETF prices.
-    benchmark (str): The ticker symbol of the benchmark.
-    Returns:
-    pd.DataFrame: A DataFrame containing the RS ratios.
-    """
-    benchmark_prices = prices[benchmark]
-    rs_ratios = pd.DataFrame()
-    for etf in prices.columns:
-        rs_ratio = prices[etf] / benchmark_prices
-        rs_ratios[etf] = rs_ratio
-
-    rs_ratios.columns = pd.MultiIndex.from_product([rs_ratios.columns, ['RS Ratio']])
-
-    return rs_ratios
-
-def load_etf_prices_from_excel(file_path, start_date, end_date):
-    """
-    Load ETF ticker symbols from Excel and download the ETF prices from Yahoo Finance.
-    Parameters:
-    file_path (str): The file path of the Excel file containing the ticker symbols.
-    start_date (str): The start date in 'YYYY-MM-DD' format.
-    end_date (str): The end date in 'YYYY-MM-DD' format.
-    Returns:
-    pd.DataFrame: A DataFrame containing the ETF prices.
-    """
-    # Load the ETF ticker symbols from Excel
-    tickers_df = pd.read_excel(file_path)
-    etfs = tickers_df['Ticker'].tolist()
-
-    # Download the ETF prices from Yahoo Finance
-    prices = pd.DataFrame()
-    for etf in etfs:
-        try:
-            data = yf.download(etf, start=start_date, end=end_date)
-            prices[etf] = data["Close"]
-        except Exception as e:
-            print(f"Error retrieving data for {etf}: {e}")
-
-    return prices
-
-
-
-
-# Default ETF names
-etf_names = ['SPY', 'SPY', 'SPY', 'SPY']
-
-def visualize_rrg(etf_names, jdk_rs_ratios, jdk_rs_momentums, rs_ratios, trailing_days=20):
-    
-    """
-    Visualize the RRG for the given ETF names.
-    Parameters:
-    etf_names (list): A list of ETF names to be visualized.
-    jdk_rs_ratios (pd.DataFrame): DataFrame containing JDK RS Ratios.
-    jdk_rs_momentums (pd.DataFrame): DataFrame containing JDK RS Momentums.
-    rs_ratios (pd.DataFrame): DataFrame containing RS Ratios.
-    trailing_days (int): Number of days in the trailing line.
-    """ 
-  
-    # Create a figure and axis object
-    fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(20, 20))
-
-    # Set the axis limits for all subplots
-    for ax in axs.flat:
-        ax.set_xlim(-0.25, 0.25)
-        ax.set_ylim(-0.25, 0.25)
-
-             
-    # Loop through each ETF and plot its data in a separate subplot
-    for i, etf in enumerate(etf_names):
-        x, y = jdk_rs_ratios[(etf, 'RS Ratio')].iloc[-1], jdk_rs_momentums[(etf, 'RS Ratio')].iloc[-1]
-        ax = axs[i // 2, i % 2]
-        ax.scatter(x, y, color='red', alpha=1.00, linewidth=2)  # use a single color for all the plots
-
-        # Create a dataframe for the last n days of data
-        trailing_data = rs_ratios.iloc[-trailing_days:]
-
-        # Plot the trailing line for each ETF
-        x_trailing = jdk_rs_ratios[(etf, 'RS Ratio')].loc[trailing_data.index]
-        y_trailing = jdk_rs_momentums[(etf, 'RS Ratio')].loc[trailing_data.index]
-        ax.plot(x_trailing, y_trailing, color='red', alpha=0.50, linewidth=2)
-
-        # Add dots on the trailing line for every 14 days
-        for j in range(0, len(x_trailing), 14):
-            ax.scatter(x_trailing.iloc[j], y_trailing.iloc[j], color='black', alpha=0.3, s=10)
-        
-        # Define the colors for each quadrant
-        colors2 = {"Leading": "darkgreen", "Lagging": "darkred", "Improving": "orange", "Weakening": "darkblue"}
-
-        # Add rectangular patches to shade the quadrants
-        ax.add_patch(mpl.patches.Rectangle((-1, -1), 1, 1, color=colors2["Lagging"], alpha=0.1))
-        ax.add_patch(mpl.patches.Rectangle((0, -1), 1, 1, color=colors2["Improving"], alpha=0.1))
-        ax.add_patch(mpl.patches.Rectangle((0, 0), 1, 1, color=colors2["Leading"], alpha=0.1))
-        ax.add_patch(mpl.patches.Rectangle((-1, 0), 1, 1, color=colors2["Weakening"], alpha=0.1))
-
-        # Add a circle at the center
-        center = (0, 0)  # center coordinates
-        radius = 0.1  # radius of the circle
-        circle = plt.Circle(center, radius, facecolor='none', edgecolor='gray', alpha=0.3, linewidth=2)
-        ax.add_patch(circle)
-                
-        # Add x-axis and y-axis lines
-        ax.axhline(y=0, color='gray')
-        ax.axvline(x=0, color='gray')
-
-        # Add labels for the quadrants
-        ax.text(-0.15, 0.15, "Improving", fontsize=8, ha="center", va="center")
-        ax.text(-0.15, -0.15, "Lagging", fontsize=8, ha="center", va="center")
-        ax.text(0.15, -0.15, "Weakening", fontsize=8, ha="center", va="center")
-        ax.text(0.15, 0.15, "Leading", fontsize=8, ha="center", va="center")
-
-        # Add legend
-        ax.legend([etf], loc="upper center", fontsize=8)
-
-
-        
-# Set the titles for each subplot
-        for i, etf in enumerate(etf_names):
-            ax = axs[i // 2, i % 2]
-            ax.set_title(etf)
-
-# Adjust spacing between subplots
-plt.tight_layout()
-
-# Show the plot
-plt.show()
-
-
-
-
 def categorize_point(x, y):
     """
     Categorizes a point based on its coordinates (x, y) into one of four categories.
@@ -242,10 +104,7 @@ def categorize_point(x, y):
     else:
         return 'Lagging'
 
-
-
-
-def categorize_signal(x, y, circle_radius):
+def categorize_radius(x, y, circle_radius):
     """
     Categorizes a signal based on its coordinates (x, y) relative to a given circle radius.
     
@@ -256,15 +115,27 @@ def categorize_signal(x, y, circle_radius):
         
     Returns:
         str: The category of the signal.
-            - 'Weak Signal' if the signal is within or on the circle boundary (distance <= circle_radius).
-            - 'Strong Signal' if the signal is outside the circle boundary (distance > circle_radius).
+            - 'Weak' if the signal is within or on the circle boundary (distance <= circle_radius).
+            - 'Strong' if the signal is outside the circle boundary (distance > circle_radius).
     """
     distance = np.sqrt(x**2 + y**2)
     if distance <= circle_radius:
-        return 'Weak Signal'
+        return 'Weak'
     else:
-        return 'Strong Signal'
+        return 'Strong'
 
+
+def calculate_slope(x1, y1, x2, y2):
+    """
+    Calculate the slope between two points.
+    Arguments:
+    x1, y1: The coordinates of the first point.
+    x2, y2: The coordinates of the second point.
+    Returns:
+    The slope between the two points.
+    """
+    slope = (y2 - y1) / (x2 - x1)
+    return slope
 
 
 
@@ -343,7 +214,7 @@ def extract_coordinates(etf_names, jdk_rs_ratios, jdk_rs_momentums, rs_ratios, t
             x_point, y_point, projected_x, projected_y, moving_towards, x_historic, y_historic, projected_category = extrapolate_coordinates(x_series, y_series, j)
 
             category = categorize_point(x_point, y_point)
-            signal = categorize_signal(x_point, y_point, circle_radius)
+            signal = categorize_radius(x_point, y_point, circle_radius)
             historic_x = x_historic
             historic_y = y_historic
 
@@ -364,4 +235,85 @@ def extract_coordinates(etf_names, jdk_rs_ratios, jdk_rs_momentums, rs_ratios, t
             }, ignore_index=True)
 
     return coordinates
+
+# Default ETF names
+def visualize_rrg(jdk_rs_ratios, jdk_rs_momentums, rs_ratios,etf_names= ['SPY', 'SPY', 'SPY', 'SPY'] , trailing_days=20):
+    
+    """
+    Visualize the RRG for the given ETF names.
+    Parameters:
+    etf_names (list): A list of ETF names to be visualized.
+    jdk_rs_ratios (pd.DataFrame): DataFrame containing JDK RS Ratios.
+    jdk_rs_momentums (pd.DataFrame): DataFrame containing JDK RS Momentums.
+    rs_ratios (pd.DataFrame): DataFrame containing RS Ratios.
+    trailing_days (int): Number of days in the trailing line.
+    """ 
+  
+    # Create a figure and axis object
+    fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(20, 20))
+
+    # Set the axis limits for all subplots
+    for ax in axs.flat:
+        ax.set_xlim(-0.25, 0.25)
+        ax.set_ylim(-0.25, 0.25)
+
+             
+    # Loop through each ETF and plot its data in a separate subplot
+    for i, etf in enumerate(etf_names):
+        x, y = jdk_rs_ratios[(etf, 'RS Ratio')].iloc[-1], jdk_rs_momentums[(etf, 'RS Ratio')].iloc[-1]
+        ax = axs[i // 2, i % 2]
+        ax.scatter(x, y, color='red', alpha=1.00, linewidth=2)  # use a single color for all the plots
+
+        # Create a dataframe for the last n days of data
+        trailing_data = rs_ratios.iloc[-trailing_days:]
+
+        # Plot the trailing line for each ETF
+        x_trailing = jdk_rs_ratios[(etf, 'RS Ratio')].loc[trailing_data.index]
+        y_trailing = jdk_rs_momentums[(etf, 'RS Ratio')].loc[trailing_data.index]
+        ax.plot(x_trailing, y_trailing, color='red', alpha=0.50, linewidth=2)
+
+        # Add dots on the trailing line for every 14 days
+        for j in range(0, len(x_trailing), 14):
+            ax.scatter(x_trailing.iloc[j], y_trailing.iloc[j], color='black', alpha=0.3, s=10)
+        
+        # Define the colors for each quadrant
+        colors2 = {"Leading": "darkgreen", "Lagging": "darkred", "Improving": "orange", "Weakening": "darkblue"}
+
+        # Add rectangular patches to shade the quadrants
+        ax.add_patch(mpl.patches.Rectangle((-1, -1), 1, 1, color=colors2["Lagging"], alpha=0.1))
+        ax.add_patch(mpl.patches.Rectangle((0, -1), 1, 1, color=colors2["Improving"], alpha=0.1))
+        ax.add_patch(mpl.patches.Rectangle((0, 0), 1, 1, color=colors2["Leading"], alpha=0.1))
+        ax.add_patch(mpl.patches.Rectangle((-1, 0), 1, 1, color=colors2["Weakening"], alpha=0.1))
+
+        # Add a circle at the center
+        center = (0, 0)  # center coordinates
+        radius = 0.1  # radius of the circle
+        circle = plt.Circle(center, radius, facecolor='none', edgecolor='gray', alpha=0.3, linewidth=2)
+        ax.add_patch(circle)
+                
+        # Add x-axis and y-axis lines
+        ax.axhline(y=0, color='gray')
+        ax.axvline(x=0, color='gray')
+
+        # Add labels for the quadrants
+        ax.text(-0.15, 0.15, "Improving", fontsize=8, ha="center", va="center")
+        ax.text(-0.15, -0.15, "Lagging", fontsize=8, ha="center", va="center")
+        ax.text(0.15, -0.15, "Weakening", fontsize=8, ha="center", va="center")
+        ax.text(0.15, 0.15, "Leading", fontsize=8, ha="center", va="center")
+
+        # Add legend
+        ax.legend([etf], loc="upper center", fontsize=8)
+
+
+        
+# Set the titles for each subplot
+        for i, etf in enumerate(etf_names):
+            ax = axs[i // 2, i % 2]
+            ax.set_title(etf)
+
+    # Adjust spacing between subplots
+    plt.tight_layout()
+
+    # Show the plot
+    plt.show()
 
